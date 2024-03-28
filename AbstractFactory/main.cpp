15b14d88f686d44c39d2e89c80ea9f07ea0d2b0a
zhangbo <mageboer@qq.com>
2024-03-28 20:42:32 +0800

    Add Adapter Method

--- AbstractFactory/main.cpp
+++ AbstractFactory/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 
-
 /**
  * Each distinct product of a product family should have a base interface. All
  * variants of the product must implement this interface.
